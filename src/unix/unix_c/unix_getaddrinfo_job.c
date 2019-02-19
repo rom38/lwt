aca27a9f9cbd250d7570c8dd5721b31d145016ed
rom38 <rom38@users.noreply.github.com>
2019-02-19 17:07:44 +0800

    Define socket_type table and socket_domain_table

--- src/unix/unix_c/unix_getaddrinfo_job.c
+++ src/unix/unix_c/unix_getaddrinfo_job.c
@@ -19,6 +19,10 @@
 #include "lwt_unix.h"
 #include "unix_recv_send_utils.h"
 
+static int socket_domain_table[] = {PF_UNIX, PF_INET};
+
+static int socket_type_table[] = {SOCK_STREAM, SOCK_DGRAM, SOCK_RAW, SOCK_SEQPACKET};
+
 struct job_getaddrinfo {
     struct lwt_unix_job job;
     char *node;
